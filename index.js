12c23c6180ce4335e6e76a1553196e9adbd8a416
cdt96 <cody.theroux3@gmail.com>
2022-06-23 15:22:13 -0400

    made my starter prompt and made all my cases for which choice is selected

--- index.js
+++ index.js
@@ -13,6 +13,35 @@ db.connect(function(err){
     if (err) throw err;
     starterPrompt();
 });
+const starterPrompt = () => {
+    inquirer.prompt({
+        type: "list",
+        name: "task",
+        message: "Which task would you like to do?",
+        choices: [
+            "View All Employees",
+            "View All Roles",
+            "View All Departments",
+            "Add An Employee",
+            "Add A Role",
+            "Add A Department",
+            "Update An Employee Role",
+            "Exit"
+        ]
+    })
+    .then(data => {
+        if (data.starterPrompt === "View All Employees") {return viewAllEmployees(); };
+        if (data.starterPrompt === "View All Roles") {return viewAllRoles(); };
+        if (data.starterPrompt === "View All Departments") {return viewAllDepartments(); };
+        if (data.starterPrompt === "Add An Employee") {return addAnEmployee(); };
+        if (data.starterPrompt === "Add A Role") {return addARole(); };
+        if (data.starterPrompt === "Add A Department") {return addADepartment(); };
+        if (data.starterPrompt === "Update An Employee Role") {return updateAnEmployeeRole(); };
+        if (data.starterPrompt === "Exit") {db.end(); };
+
+    })
+
+}
 
 //need starter prompt here
 
