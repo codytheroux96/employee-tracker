2ac6339347d852599c23de19fa3fedbb80de2f17
cdt96 <cody.theroux3@gmail.com>
2022-06-23 13:39:13 -0400

    created my tables in schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,24 @@
 DROP DATABASE IF EXISTS employeesDB;
 CREATE DATABASE employeesDB;
 USE employeesDB;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(12,2) NOT NULL,
+    department_id INT NOT NULL,
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NULL,
+    manager_id INT NULL,
+    PRIMARY KEY (id),
+);
