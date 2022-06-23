107e3e5ebd5f928778ecedb0a8548492be7255d9
cdt96 <cody.theroux3@gmail.com>
2022-06-23 14:33:48 -0400

    added a manager id to the seeds for employee

--- db/seeds.sql
+++ db/seeds.sql
@@ -21,25 +21,25 @@ INSERT INTO role (title, salary, department_id)
 VALUES ("General Manager", 80000, 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES("Celix", "Ramos", 1);
+VALUES("Celix", "Ramos", 1, 3);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES("Cody", "Fincher", 1);
+VALUES("Cody", "Fincher", 1, 3);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES("Jerome", "Clyburn", 4);
+VALUES("Jerome", "Clyburn", 4, 10);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES("Veronica", "Stringfield", 2);
+VALUES("Veronica", "Stringfield", 3, 6);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES("Cami", "Jenkins", 2);
+VALUES("Cami", "Jenkins", 3, 6);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES("Cody", "Theroux", 5);
+VALUES("Cody", "Theroux", 5, 10);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES("Hunter", "Ueberoth", 2);
+VALUES("Hunter", "Ueberoth", 2, 7);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES("Taniya", "Hargrove", 2);
+VALUES("Taniya", "Hargrove", 2, 7);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES("Candice", "Hotte", 5);
+VALUES("Candice", "Hotte", 5, 10);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES("Gregg", "Bibik", 6);
+VALUES("Gregg", "Bibik", 6, NULL);
 
 
     
