ac9d6d7a211380a4bdda824f6e4ec52d2fb22421
cdt96 <cody.theroux3@gmail.com>
2022-06-24 10:34:58 -0400

    switched to switch cases instead of if then statements

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
-const table = require("console.table");
+require("console.table");
 const { updateAnEmployeeRole } = require("./utils/update");
 const { viewAllEmployees, viewAllRoles, viewAllDepartments} = require("./utils/view");
 const { addAnEmployee, addARole, addADepartment} = require("./utils/add");
@@ -10,7 +10,8 @@ const db = mysql.createConnection({
       user: "root",
       password: '',
       database: "employeesDB",
-});
+},
+console.log(`Connected to the employeesDB`));
 
 db.connect(function(err){
     if (err) throw err;
@@ -32,17 +33,67 @@ const starterPrompt = () => {
             "Exit"
         ]
     })
-    .then(data => {
-        if (data.starterPrompt === "View All Employees") {return viewAllEmployees(); };
-        if (data.starterPrompt === "View All Roles") {return viewAllRoles(); };
-        if (data.starterPrompt === "View All Departments") {return viewAllDepartments(); };
-        if (data.starterPrompt === "Add An Employee") {return addAnEmployee(); };
-        if (data.starterPrompt === "Add A Role") {return addARole(); };
-        if (data.starterPrompt === "Add A Department") {return addADepartment(); };
-        if (data.starterPrompt === "Update An Employee Role") {return updateAnEmployeeRole(); };
-        if (data.starterPrompt === "Exit") {db.end(); };
-
+    .then((answers) => {
+        switch (answers.task){
+            case "View All Employees": {
+                viewAllEmployees();
+                break;
+            }
+        }
+        switch (answers.task){
+            case "View All Roles": {
+                viewAllRoles();
+                break;
+            }
+        }
+        switch (answers.task){
+            case "View All Departments": {
+                viewAllDepartments();
+                break;
+            }
+        }
+        switch (answers.task){
+            case "Add An Employee": {
+                addAnEmployee();
+                break;
+            }
+        }
+        switch (answers.task){
+            case "Add A Role": {
+                addARole();
+                break;
+            }
+        }
+        switch (answers.task){
+            case "Add A Department": {
+                addADepartment();
+                break;
+            }
+        }
+        switch (answers.task){
+            case "Update An Employee Role": {
+                updateAnEmployeeRole();
+                break;
+            }
+        }
+        switch (answers.task){
+            case "Exit": {
+                db.end();
+                break;
+            }
+        }
     })
+    // .then(data => {
+    //     if (data.starterPrompt === "View All Employees") {return viewAllEmployees(); };
+    //     if (data.starterPrompt === "View All Roles") {return viewAllRoles(); };
+    //     if (data.starterPrompt === "View All Departments") {return viewAllDepartments(); };
+    //     if (data.starterPrompt === "Add An Employee") {return addAnEmployee(); };
+    //     if (data.starterPrompt === "Add A Role") {return addARole(); };
+    //     if (data.starterPrompt === "Add A Department") {return addADepartment(); };
+    //     if (data.starterPrompt === "Update An Employee Role") {return updateAnEmployeeRole(); };
+    //     if (data.starterPrompt === "Exit") {db.end(); };
+
+    // })
 
 }
 
